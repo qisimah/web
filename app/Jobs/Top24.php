45c7ef8c53c35132139b23a95115d41208132e20
Solomon Appier-Sign <braasig@gmail.com>
2017-11-27 16:46:36 +0000

    Chart entry calculation between date ranges

--- app/Jobs/Top24.php
+++ app/Jobs/Top24.php
@@ -13,18 +13,19 @@ use Illuminate\Support\Facades\DB;
 class Top24
 {
     private $top24;
-
     private $country_id;
+    private $date;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($country_id)
+    public function __construct($country_id, $date)
     {
         $this->top24 = new \App\Top24();
         $this->country_id = $country_id;
+        $this->date = $date;
     }
 
     /**
@@ -36,14 +37,21 @@ class Top24
     {
         $position = 1;
         $stream_ids = Broadcaster::getBroadcastersStreamIdsForCountry($this->country_id);
-        $plays =  Play::select(DB::raw('file_id, count(*) as plays'))->whereIn('stream_id', $stream_ids)->whereDate('created_at', Carbon::today()->toDateString())->with('file')->groupBy('file_id')->orderBy('plays', 'desc')->limit(24)->get();
+        $plays =  Play::select(DB::raw('file_id, count(*) as plays'))->whereIn('stream_id', $stream_ids)->whereDate('created_at', $this->date)->with('file')->groupBy('file_id')->orderBy('plays', 'desc')->limit(24)->get();
         foreach ($plays as $play) {
             $entry = new ChartEntry($play->file_id, $play->file->title, Chart::artistsNamesToString(File::allArtists($play->file)), Chart::arraysToString($play->file->producers()->pluck('nick_name')->toArray()), Chart::arraysToString($play->file->genres()->pluck('name')->toArray()), $play->file->release_date, $play->file->img, $play->file->audio, $play->plays, $position, 0, 1, $this->country_id, Carbon::today()->toDateString());
-            $entry->getEntry()['duration']      = $entry->duration();
-            $entry->getEntry()['peak_position'] = $entry->peakPosition();
-            $entry->getEntry()['prev_position'] = $entry->previousPosition();
+            $entry->setDuration($entry->duration());
+            $entry->setPeakPosition($entry->peakPosition());
+            $entry->setPreviousPosition($entry->previousPosition());
             $this->top24->create($entry->getEntry());
             $position++;
         }
+
+//        $start = Carbon\Carbon::create(2017, 7, 1);
+//        $days = Carbon\Carbon::today()->diffInDays($start);
+//        for ($i = 0; $i <= $days; $i++){
+//            echo $i."\n";
+//            echo $i."\r". Carbon\Carbon::create(2017, 7, 1)->addDays($i)->toDateString()."\n";
+//        }
     }
 }
