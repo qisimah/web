2c9b4bb9e99eeef22ed71a4eadd66a42da10b685
Solomon Appier-Sign <sappiah@payswitch.com.gh>
2017-12-28 17:24:03 +0000

    broadcaster edit completed

--- app/Tag.php
+++ app/Tag.php
@@ -6,7 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtolower($value);
+    }
+
+    public function getNameAttribute($value)
+    {
+        return $this->attributes['name'] = ucwords($value);
+    }
+
     public function broadcasters()
     {
         return $this->belongsToMany(Broadcaster::class);
